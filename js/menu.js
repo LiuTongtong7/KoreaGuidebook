bcb7ae1447f0fa6829235af471416a3b6a6fe85c
LiuTongtong7 <liutongtong6@me.com>
2015-05-31 15:24:22 +0800

    add menus to all pages

--- js/menu.js
+++ js/menu.js
@@ -6,10 +6,14 @@ $(document).ready(function() {
     $(".menu-button").bind("click", function() {
         if ($(".menu").hasClass("menu-active")) {
             $(".archives").removeClass("archives-inactive");
+            $(".slices").removeClass("slices-inactive");
+            $(".slice-nav").removeClass("slice-nav-inactive");
             $(".menu").removeClass("menu-active");
             enable_archive_click();
         } else {
             $(".archives").addClass("archives-inactive");
+            $(".slices").addClass("slices-inactive");
+            $(".slice-nav").addClass("slice-nav-inactive");
             $(".menu").addClass("menu-active");
             disable_archive_click();
         }
