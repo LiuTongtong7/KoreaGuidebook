8d80fe113a0d65c33666dd22faddcf6aec379769
LiuTongtong7 <liutongtong6@me.com>
2015-05-31 17:16:11 +0800

    complete menu

--- js/menu.js
+++ js/menu.js
@@ -5,17 +5,25 @@
 $(document).ready(function() {
     $(".menu-button").bind("click", function() {
         if ($(".menu").hasClass("menu-active")) {
-            $(".archives").removeClass("archives-inactive");
-            $(".slices").removeClass("slices-inactive");
-            $(".slice-nav").removeClass("slice-nav-inactive");
+            if ($(".archives").length > 0) {
+                $(".archives").removeClass("archives-inactive");
+                enable_archive_click();
+            }
+            if ($(".slices").length > 0) {
+                $(".slices").removeClass("slices-inactive");
+                $(".slice-nav").removeClass("slice-nav-inactive");
+            }
             $(".menu").removeClass("menu-active");
-            enable_archive_click();
         } else {
-            $(".archives").addClass("archives-inactive");
-            $(".slices").addClass("slices-inactive");
-            $(".slice-nav").addClass("slice-nav-inactive");
+            if ($(".archives").length > 0) {
+                $(".archives").addClass("archives-inactive");
+                disable_archive_click();
+            }
+            if ($(".slices").length > 0) {
+                $(".slices").addClass("slices-inactive");
+                $(".slice-nav").addClass("slice-nav-inactive");
+            }
             $(".menu").addClass("menu-active");
-            disable_archive_click();
         }
     });
 });
